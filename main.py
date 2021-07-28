1cb397b78c8336a357fda29344086c8171ac7644
SedepK <88108222+SedepK@users.noreply.github.com>
2021-07-28 21:11:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def difference ():
-    import random as d 
-    m = [d.randint (10, 47) for i in range (57)]
+    import random as d
+    difference = [m[i + 1] - m[i] for i in range(len(m) - 1)]
     difference = [m [i +1] - m[i] for i in range (len (m) -1)]
     return(difference)
 print (difference())
