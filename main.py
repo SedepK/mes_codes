5059153aa5813df3721b1ba8a2fa58e25d69b192
SedepK <88108222+SedepK@users.noreply.github.com>
2021-09-27 14:57:17 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-def difference ():
-    import random as d
-    difference = [m[i + 1] - m[i] for i in range(len(m) - 1)]
-    difference = [m [i +1] - m[i] for i in range (len (m) -1)]
-    return(difference)
-print (difference())
